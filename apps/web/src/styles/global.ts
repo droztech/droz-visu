c8ed4cf0361a40308fcab212de62c1c61d6d6cf6
YaGRRusso <yagrrusso@gmail.com>
2023-01-20 16:29:59 -0300

    web config

--- apps/web/src/styles/global.ts
+++ apps/web/src/styles/global.ts
@@ -1,16 +1,5 @@
 import { globalCss } from "@stitches/react";
 
-export const componentsStyle = globalCss({
-  "*": {
-    fontSize: "1rem",
-    boxSizing: "border-box",
-  },
-  "*:focus:not(:active)": {
-    outline: "0.2rem solid $gray900",
-    outlineOffset: "0.2rem",
-  },
-});
-
 export const appStyle = globalCss({
   "@import":
     "url('https://fonts.googleapis.com/css2?family=Kanit&display=swap')",
--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,17 +1,13 @@
 import * as Component from "@styles/index";
-import { Button } from "@library";
-import { CheckCircle } from "phosphor-react";
+import { Button } from "./library";
 
 function App() {
   return (
     <Component.Main>
       <Component.Container>
-        <Button.Root>
-          <Button.Icon>
-            <CheckCircle />
-          </Button.Icon>
-          Clique aqui
-        </Button.Root>
+        {/* ========================= TEST AREA ========================= */}
+
+        {/* ========================= TEST AREA ========================= */}
       </Component.Container>
     </Component.Main>
   );
