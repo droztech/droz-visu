f54efbac1944f68a6ecd973eb19c7b1863653c43
YaGRRusso <yagrrusso@gmail.com>
2023-01-26 14:23:02 -0300

    input test

--- apps/web/__tests__/input.test.tsx
+++ apps/web/__tests__/input.test.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@coaktion/visu";
-import { render } from "@testing-library/react";
-import { Envelope, Info } from "phosphor-react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Envelope } from "phosphor-react";
 
 describe("Input tests", () => {
   const inputEmail = "email@email.com";
@@ -24,6 +24,24 @@ describe("Input tests", () => {
     );
     const element = container.firstChild as Element;
 
-    expect(element).toBeDefined();
+    fireEvent.change(element.firstChild as Element, {
+      target: { value: inputEmail },
+    });
+    expect(element.firstChild).toHaveValue(inputEmail);
   });
+
+  // it("Should select input on icon click", () => {
+  //   const { container } = render(
+  //     <Input.Root>
+  //       <Input.Icon>
+  //         <Envelope />
+  //       </Input.Icon>
+  //       <Input.Input data-testid="input" />
+  //     </Input.Root>
+  //   );
+  //   const element = container.firstChild as Element;
+
+  //   fireEvent.click(element.firstChild as Element);
+  //   expect(screen.getByTestId("input")).toHaveFocus();
+  // });
 });
