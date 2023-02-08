4ebe122b4de5e522b99fc6b57b504e8dadba8f15
Leandro Pedroso <lpedroso@meudroz.com>
2023-02-08 20:35:07 -0300

    Campo de testes

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,32 +1,16 @@
-import { Input } from '@library'
-import { Eye, EyeSlash, Info } from 'phosphor-react'
-import { useState } from 'react'
+import { Text } from '@library'
 
 import * as Component from '@styles/index'
 
 function App() {
-  const [isShowingPassword, setIsShowingPassword] = useState(false)
-
   return (
     <Component.Main>
       <Component.Container>
         {/* ========================= TEST AREA ========================= */}
 
-        <Input.Root>
-          <Input.Icon
-            onClick={() => setIsShowingPassword(!isShowingPassword)}
-            position="left"
-          >
-            {isShowingPassword ? <Eye /> : <EyeSlash />}
-          </Input.Icon>
-          <Input.Icon position="right">
-            <Info />
-          </Input.Icon>
-          <Input.Input
-            type={isShowingPassword ? 'text' : 'password'}
-            placeholder="Digite sua senha aqui"
-          />
-        </Input.Root>
+        <Text color="primary" link="primary" size="3xl">
+          Hello World
+        </Text>
 
         {/* ========================= TEST AREA ========================= */}
       </Component.Container>
