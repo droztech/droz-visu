aef056cc5aa01c10f77f7fcc2a36056bc01998d3
Leandro Pedroso <lpedroso@meudroz.com>
2023-02-09 11:34:23 -0300

    Testes para componentes de Botão

--- apps/web/__tests__/Button/Root.test.tsx
+++ apps/web/__tests__/Button/Root.test.tsx
@@ -3,25 +3,25 @@ import { CheckCircle } from 'phosphor-react'
 
 import { Button } from '@coaktion/visu'
 
-describe('Button tests', () => {
+describe('Button Root tests', () => {
   const buttonText = 'Clique aqui'
   const buttonClick = jest.fn()
 
-  it('Should render a button', () => {
+  it('Should render a button element', () => {
     render(<Button.Root data-testid="element">{buttonText}</Button.Root>)
     const element = screen.getByTestId('element')
 
     expect(element).toBeDefined()
   })
 
-  it('Should render a button with correct text', () => {
+  it('Should render a button element with a child text', () => {
     render(<Button.Root data-testid="element">{buttonText}</Button.Root>)
     const element = screen.getByTestId('element')
 
     expect(element).toHaveTextContent(buttonText)
   })
 
-  it('Should render a button with JSX element', () => {
+  it('Should render a button element with a child JSX element', () => {
     render(
       <Button.Root data-testid="element">
         <span>{buttonText}</span>
@@ -33,39 +33,7 @@ describe('Button tests', () => {
     expect(element.firstElementChild).toHaveTextContent(buttonText)
   })
 
-  it('Should render a button with icon and text', () => {
-    render(
-      <Button.Root data-testid="element">
-        <Button.Icon>
-          <CheckCircle />
-        </Button.Icon>
-        {buttonText}
-      </Button.Root>
-    )
-    const element = screen.getByTestId('element')
-
-    expect(element.firstElementChild?.firstElementChild?.tagName).toBe('svg')
-  })
-
-  it('Should render a button with size styles', () => {
-    render(
-      <Button.Root data-testid="element" ghost>
-        {buttonText}
-      </Button.Root>
-    )
-    const element = screen.getByTestId('element')
-  })
-
-  it('Should render a button with ghost style', () => {
-    render(
-      <Button.Root data-testid="element" ghost>
-        {buttonText}
-      </Button.Root>
-    )
-    const element = screen.getByTestId('element')
-  })
-
-  it('Should render a button with disabled style and unable to click', () => {
+  it('Should render a button element with disabled style and unable to click', () => {
     render(
       <Button.Root data-testid="element" disabled>
         {buttonText}
@@ -77,7 +45,7 @@ describe('Button tests', () => {
     expect(buttonClick).toHaveBeenCalledTimes(0)
   })
 
-  it('Should render a button with onClick', () => {
+  it('Should render a button element with onClick', () => {
     render(
       <Button.Root data-testid="element" onClick={buttonClick}>
         {buttonText}
