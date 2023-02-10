42ad3617d1a0e4bfa78be98ad5b241c2118c821b
Leandro Pedroso <lpedroso@meudroz.com>
2023-02-10 09:11:20 -0300

    Remoção de conteúdo da área de testes

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,6 +1,3 @@
-import { Button, Text } from '@library'
-import { Envelope } from 'phosphor-react'
-
 import * as Component from '@styles/index'
 
 function App() {
@@ -8,15 +5,6 @@ function App() {
     <Component.Main>
       <Component.Container>
         {/* ========================= TEST AREA ========================= */}
-
-        <Text color="primary" link="primary" size="3xl" asChild>
-          <a href="/">E ai</a>
-        </Text>
-
-        <Button.Root>
-          <span>Opa</span>
-          <Envelope size={32} />
-        </Button.Root>
 
         {/* ========================= TEST AREA ========================= */}
       </Component.Container>
