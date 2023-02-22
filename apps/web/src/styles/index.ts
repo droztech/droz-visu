d996f9e2dbab0dc4b9491a7945b5fb3e763e4f4e
Leandro Pedroso <lpedroso@meudroz.com>
2023-02-22 14:49:23 -0300

    Adicionado dark theme ao projeto web

--- apps/web/src/styles/index.ts
+++ apps/web/src/styles/index.ts
@@ -1,12 +1,23 @@
-import { styled } from '@stitches/react'
+import { styled } from '../../stitches.config'
 
 export const Main = styled('div', {
   display: 'flex',
-  justifyContent: 'center',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
   alignItems: 'center',
-  minHeight: '100vh',
   backgroundColor: '$background',
   color: '$gray50',
+  height: '100vh',
+  width: '100vw',
+})
+
+export const Header = styled('header', {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  width: '100%',
+  padding: '$2',
 })
 
 export const Container = styled('div', {
@@ -17,4 +28,5 @@ export const Container = styled('div', {
   gap: '1rem',
   maxWidth: '768px',
   width: '95vw',
+  height: '100%',
 })
