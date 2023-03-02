afd0f0d314c1b78e5c78267fbe5dd391edc4a968
YaGRRusso <yagrrusso@gmail.com>
2023-03-02 18:52:50 -0300

    more tests

--- packages/visu/src/__tests__/Flex/Row.test.tsx
+++ packages/visu/src/__tests__/Flex/Row.test.tsx
@@ -15,6 +15,22 @@ describe('Flex tests', () => {
     expect(element).toBeDefined()
   })
 
+  it('Should render elements under Flex row element', () => {
+    render(
+      <Flex.Row data-testid="element">
+        <div>First</div>
+        <span>Second</span>
+        <h1>Third</h1>
+      </Flex.Row>
+    )
+    const element = screen.getByTestId('element')
+
+    expect(element.children.length).toBe(3)
+    expect(element.children[0].tagName).toBe('DIV')
+    expect(element.children[1].tagName).toBe('SPAN')
+    expect(element.children[2].tagName).toBe('H1')
+  })
+
   it('Should render a different element using the "asChild" property', () => {
     render(
       <Flex.Row data-testid="element" asChild>
