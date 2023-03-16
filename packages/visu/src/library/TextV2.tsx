5a893b693f32a31f72345e97da0db960b1b674db
Leandro Pedroso <lpedroso@meudroz.com>
2023-03-16 12:14:11 -0300

    Melhorias de performance no componente TextV2

--- packages/visu/src/library/TextV2.tsx
+++ packages/visu/src/library/TextV2.tsx
@@ -2,8 +2,6 @@ import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
 import { HTMLAttributes, useMemo } from 'react'
 
-// ========================= ROOT =========================
-
 export interface TextProps extends HTMLAttributes<HTMLSpanElement> {
   align?: 'left' | 'center' | 'right'
   asChild?: boolean
@@ -33,75 +31,85 @@ const TextV2 = ({
   const Component = asChild ? Slot : 'span'
 
   const alignClass = useMemo(() => {
-    return clsx({
-      'text-left': align === 'left',
-      'text-center': align === 'center',
-      'text-right': align === 'right',
-    })
-  }, [])
+    return align
+      ? clsx({
+          'text-left': align === 'left',
+          'text-center': align === 'center',
+          'text-right': align === 'right',
+        })
+      : ''
+  }, [align])
 
   const colorClass = useMemo(() => {
-    return clsx({
-      'text-primary': color === 'primary',
-      'text-secondary': color === 'secondary',
-      'text-success': color === 'success',
-      'text-error': color === 'error',
-      'text-alert': color === 'alert',
-    })
-  }, [])
+    return color
+      ? clsx({
+          'text-primary': color === 'primary',
+          'text-secondary': color === 'secondary',
+          'text-success': color === 'success',
+          'text-error': color === 'error',
+          'text-alert': color === 'alert',
+        })
+      : ''
+  }, [color])
 
   const linkClass = useMemo(() => {
-    return clsx(link && ['cursor-pointer', 'rounded-lg'], {
-      'hover:text-primary-500 active:text-primary-600': link === 'primary',
-      'hover:text-secondary-500 active:text-secondary-600':
-        link === 'secondary',
-      'hover:text-success-500 active:text-success-600': link === 'success',
-      'hover:text-error-500 active:text-error-600': link === 'error',
-      'hover:text-alert-500 active:text-alert-600': link === 'alert',
-    })
-  }, [])
+    return link
+      ? clsx(['cursor-pointer', 'rounded-lg'], {
+          'hover:text-primary-500 active:text-primary-600': link === 'primary',
+          'hover:text-secondary-500 active:text-secondary-600':
+            link === 'secondary',
+          'hover:text-success-500 active:text-success-600': link === 'success',
+          'hover:text-error-500 active:text-error-600': link === 'error',
+          'hover:text-alert-500 active:text-alert-600': link === 'alert',
+        })
+      : ''
+  }, [link])
 
   const ellipsisClass = useMemo(() => {
-    return clsx(
-      ellipsis && [
-        'text-ellipsis',
-        'overflow-hidden',
-        'whitespace-nowrap',
-        'max-w-full',
-      ]
-    )
-  }, [])
+    return ellipsis
+      ? [
+          'text-ellipsis',
+          'overflow-hidden',
+          'whitespace-nowrap',
+          'max-w-full',
+        ].join(' ')
+      : ''
+  }, [ellipsis])
 
   const inlineClass = useMemo(() => {
-    return clsx(
-      inline && ['inline-flex', 'gap-px', 'flex-wrap', 'items-center']
-    )
-  }, [])
+    return inline
+      ? ['inline-flex', 'gap-px', 'flex-wrap', 'items-center'].join(' ')
+      : ''
+  }, [inline])
 
   const sizeClass = useMemo(() => {
-    return clsx({
-      'text-xs': size === 'xs',
-      'text-sm': size === 'sm',
-      'text-md': size === 'md',
-      'text-lg': size === 'lg',
-      'text-xl': size === 'xl',
-      'text-2xl': size === '2xl',
-      'text-3xl': size === '3xl',
-    })
-  }, [])
+    return size
+      ? clsx({
+          'text-xs': size === 'xs',
+          'text-sm': size === 'sm',
+          'text-md': size === 'md',
+          'text-lg': size === 'lg',
+          'text-xl': size === 'xl',
+          'text-2xl': size === '2xl',
+          'text-3xl': size === '3xl',
+        })
+      : ''
+  }, [size])
 
   const underlineClass = useMemo(() => {
-    return clsx(underline && ['underline', 'underline-offset-2'])
-  }, [])
+    return underline ? ['underline', 'underline-offset-2'].join(' ') : ''
+  }, [underline])
 
   const weightClass = useMemo(() => {
-    return clsx({
-      'font-light': weight === 'light',
-      'font-normal': weight === 'normal',
-      'font-semibold': weight === 'semibold',
-      'font-bold': weight === 'bold',
-    })
-  }, [])
+    return weight
+      ? clsx({
+          'font-light': weight === 'light',
+          'font-normal': weight === 'normal',
+          'font-semibold': weight === 'semibold',
+          'font-bold': weight === 'bold',
+        })
+      : ''
+  }, [weight])
 
   return (
     <Component
@@ -128,7 +136,5 @@ const TextV2 = ({
 }
 
 TextV2.displayName = 'Text'
-
-// ========================= EXPORTS =========================
 
 export default TextV2
