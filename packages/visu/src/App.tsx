ea66b07d07bfefff4a2e85a2f73aa129dd69ebc7
Leandro Pedroso <lpedroso@meudroz.com>
2023-03-20 12:57:52 -0300

    Reestilização da área de testes do Visu com tailwindcss

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,14 +1,28 @@
-import * as Component from '@styles/index'
+import { Flex } from '@library'
+import clsx from 'clsx'
 
 function App() {
   return (
-    <Component.Main>
-      <Component.Container>
+    <Flex.Root center className="min-h-screen bg-gray-800">
+      <Flex.Col
+        center
+        className={clsx([
+          'max-w-[95vw]',
+          'min-w-[60vw]',
+          'min-h-[40vh]',
+          'rounded-lg',
+          'border-2',
+          'border-solid',
+          'border-gray-500',
+          'bg-backgroundColor',
+          'p-16',
+        ])}
+      >
         {/* ========================= TEST AREA ========================= */}
 
         {/* ========================= TEST AREA ========================= */}
-      </Component.Container>
-    </Component.Main>
+      </Flex.Col>
+    </Flex.Root>
   )
 }
 
