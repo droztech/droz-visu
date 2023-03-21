8a3044c5b29530d5ba22569881de052bfd9ddf8b
Leandro Pedroso <lpedroso@meudroz.com>
2023-03-21 00:27:54 -0300

    Correção de utilização do Visu 2.0 no Web

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,8 @@
+import { clsx } from 'clsx'
 import { Moon, Sun } from 'phosphor-react'
 import { useCallback, useState } from 'react'
 
-import { Button } from '@coaktion/visu'
-
-import { darkTheme } from '../visu.config'
-import * as C from './styles/index'
+import { Button, Flex } from '@coaktion/visu'
 
 function App() {
   const [theme, setTheme] = useState('')
@@ -14,22 +12,35 @@ function App() {
       setTheme('')
       return
     }
-    setTheme(darkTheme)
+    setTheme('darkTheme')
   }, [theme])
 
   return (
-    <C.Main className={theme}>
-      <C.Header>
+    <Flex.Col
+      className={clsx([
+        'bg-backgroundColor',
+        'text-gray-100',
+        'h-screen',
+        'w-screen',
+      ])}
+      justify="start"
+      items="center"
+    >
+      <Flex.Row
+        className={clsx(['w-full', 'p-2'])}
+        items="center"
+        justify="end"
+      >
         <Button.Root onClick={handleChangeTheme} light>
           <Button.Icon>{theme ? <Sun /> : <Moon />}</Button.Icon>
         </Button.Root>
-      </C.Header>
-      <C.Container>
+      </Flex.Row>
+      <Flex.Col className={clsx(['max-w-3xl', 'w-[95vw]', 'h-full'])} center>
         {/* ========================= TEST AREA ========================= */}
 
         {/* ========================= TEST AREA ========================= */}
-      </C.Container>
-    </C.Main>
+      </Flex.Col>
+    </Flex.Col>
   )
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
-import { appStyle } from './styles/global'
+import '@coaktion/visu/styles'
 
-appStyle()
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
