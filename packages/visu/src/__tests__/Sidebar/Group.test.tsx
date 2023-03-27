3dd3591159c3c1c7f57eec3435bc6ed4e822914f
YaGRRusso <yagrrusso@gmail.com>
2023-03-27 17:13:02 -0300

    test

---
+++ packages/visu/src/__tests__/Sidebar/Group.test.tsx
@@ -0,0 +1,30 @@
+import { Sidebar } from '@library'
+import { render, screen } from '@testing-library/react'
+import clsx from 'clsx'
+
+jest.mock('clsx', () => {
+  return {
+    clsx: jest.fn().mockImplementation(() => clsx),
+  }
+})
+
+describe('SidebarGroup tests', () => {
+  it('Should render a SidebarGroup element', () => {
+    render(<Sidebar.Group data-testid="element"></Sidebar.Group>)
+    const element = screen.getByTestId('element')
+
+    expect(element).toBeDefined()
+  })
+
+  it('Should render a SidebarGroup element with a child', () => {
+    render(
+      <Sidebar.Group data-testid="element">
+        <div></div>
+        <div></div>
+      </Sidebar.Group>
+    )
+    const element = screen.getByTestId('element')
+
+    expect(element.children.length).toBe(2)
+  })
+})
---
+++ packages/visu/src/__tests__/Sidebar/Root.test.tsx
@@ -0,0 +1,50 @@
+import { Sidebar } from '@library'
+import { render, screen } from '@testing-library/react'
+import clsx from 'clsx'
+
+jest.mock('clsx', () => {
+  return {
+    clsx: jest.fn().mockImplementation(() => clsx),
+  }
+})
+
+describe('SidebarRoot tests', () => {
+  it('Should render a SidebarRoot element', () => {
+    render(<Sidebar.Root data-testid="element"></Sidebar.Root>)
+    const element = screen.getByTestId('element')
+
+    expect(element).toBeDefined()
+  })
+
+  it('Should render a SidebarRoot element without expand button', () => {
+    render(<Sidebar.Root data-testid="element"></Sidebar.Root>)
+    const element = screen.getByTestId('element')
+
+    expect(element.children.length).toBe(0)
+  })
+
+  it('Should render a SidebarRoot element with expand button', () => {
+    render(
+      <Sidebar.Root
+        data-testid="element"
+        setExpanded={jest.fn()}
+      ></Sidebar.Root>
+    )
+    const element = screen.getByTestId('element')
+
+    expect(element.children[0].tagName).toBe('DIV')
+    expect(element.children[0].children[0].tagName).toBe('svg')
+  })
+
+  it('Should render a SidebarRoot element with a child', () => {
+    render(
+      <Sidebar.Root data-testid="element">
+        <div></div>
+        <div></div>
+      </Sidebar.Root>
+    )
+    const element = screen.getByTestId('element')
+
+    expect(element.children.length).toBe(2)
+  })
+})
