0345532dc03ab62b95c8c8825b034d39a2c4b246
Yago Russo <yagrrusso@gmail.com>
2023-03-29 15:31:13 -0300

    Feat loading (#42)
    * loading comp
    
    * folder components
    
    * loading test
    
    * storybook and ts fixes

---
+++ packages/visu/src/__tests__/Loading/Loading.test.tsx
@@ -0,0 +1,25 @@
+import { Loading } from '@library'
+import { render, screen } from '@testing-library/react'
+import clsx from 'clsx'
+
+jest.mock('clsx', () => {
+  return {
+    clsx: jest.fn().mockImplementation(() => clsx),
+  }
+})
+
+describe('Loading tests', () => {
+  it('Should render a Loading element', () => {
+    render(<Loading data-testid="element" />)
+    const element = screen.getByTestId('element')
+
+    expect(element).toBeDefined()
+  })
+
+  it('Should render a Loading element with 3 dots', () => {
+    render(<Loading data-testid="element" />)
+    const element = screen.getByTestId('element')
+
+    expect(element.children.length).toBe(3)
+  })
+})
