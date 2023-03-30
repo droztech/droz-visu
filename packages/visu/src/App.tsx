588207d18a2bd8dab500f869e32f6ba35b5a85e9
YaGRRusso <yagrrusso@gmail.com>
2023-03-30 18:51:51 -0300

    steps tooltip

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,8 +1,6 @@
-import { Flex } from '@library'
+import { Flex, Tooltip } from '@library'
 import { clsx } from 'clsx'
 import { Info } from 'phosphor-react'
-
-import Tooltip from './library/Tooltip'
 
 function App() {
   return (
@@ -22,7 +20,7 @@ function App() {
         ])}
       >
         {/* ========================= TEST AREA ========================= */}
-        {/* <Tooltip.Step
+        <Tooltip.Step
           text={[
             'Este é um exemplo de explicação para Tooltip.',
             'dawdfawf waffawf awfawf',
@@ -30,16 +28,7 @@ function App() {
           ]}
         >
           <Info size={36} />
-        </Tooltip.Step> */}
-        <div className="bg-background shadow-sm rounded-md p-6 max-w-xs flex gap-2 items-center">
-          <span className="text-sm flex-1">
-            Este é um exemplo de explicação para Tooltip.
-          </span>
-          <button className="bg-gray-300 rounded-full flex items-center justify-center text-primary hover:bg-primary hover:text-gray-100 active:bg-primary-500 active:text-gray-100 transition-all">
-            Próximo
-            <X size={12} />
-          </button>
-        </div>
+        </Tooltip.Step>
         {/* ========================= TEST AREA ========================= */}
       </Flex.Col>
     </Flex.Root>
--- packages/visu/src/library/Tooltip/Step.tsx
+++ packages/visu/src/library/Tooltip/Step.tsx
@@ -1,7 +1,7 @@
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 import clsx from 'clsx'
-import { ArrowRight } from 'phosphor-react'
-import { FC, HTMLAttributes, useMemo, useState } from 'react'
+import { ArrowCounterClockwise, ArrowRight, X } from 'phosphor-react'
+import { FC, HTMLAttributes, useCallback, useMemo, useState } from 'react'
 
 export interface TooltipStepProps extends HTMLAttributes<HTMLDivElement> {
   side?: 'top' | 'right' | 'bottom' | 'left' | undefined
@@ -25,6 +25,11 @@ const TooltipStep: FC<TooltipStepProps> = ({
     [trigger, isOpen]
   )
 
+  const handleClose = useCallback(() => {
+    setCurrentStep(1)
+    setIsOpen(false)
+  }, [])
+
   return (
     <>
       {open && (
@@ -45,16 +50,35 @@ const TooltipStep: FC<TooltipStepProps> = ({
           <RadixTooltip.Portal className="hidden">
             <RadixTooltip.Content side={side} sideOffset={5}>
               <RadixTooltip.Arrow className="fill-background w-5 h-2" />
-              <div className="bg-background shadow-sm rounded-md p-3 max-w-xs">
+              <div className="bg-background shadow-sm rounded-md p-6 max-w-xs flex flex-col gap-4 items-center">
                 <span className={clsx('text-sm', className)} {...rest}>
                   {text[currentStep - 1]}
                 </span>
-                <button
-                  className="bg-gray-300 rounded-full flex items-center justify-center h-6 w-6 text-primary hover:bg-primary hover:text-gray-100 active:bg-primary-500 active:text-gray-100 transition-all"
-                  onClick={() => setCurrentStep(currentStep + 1)}
-                >
-                  <ArrowRight size={12} />
-                </button>
+                <div className="flex justify-between items-center w-full gap-2">
+                  <span className="text-gray-500 text-sm">
+                    Step {currentStep}/{text.length}
+                  </span>
+                  {currentStep === text.length ? (
+                    <button
+                      className="bg-gray-300 rounded-full flex items-center justify-center h-6 w-6 text-primary hover:bg-primary hover:text-gray-100 active:bg-primary-500 active:text-gray-100 transition-all"
+                      onClick={handleClose}
+                    >
+                      {trigger === 'click' ? (
+                        <X size={12} />
+                      ) : (
+                        <ArrowCounterClockwise size={12} />
+                      )}
+                    </button>
+                  ) : (
+                    <button
+                      className="bg-gray-300 text-sm rounded-full px-4 py-1 flex gap-2 items-center justify-center text-primary hover:bg-primary hover:text-gray-100 active:bg-primary-500 active:text-gray-100 transition-all"
+                      onClick={() => setCurrentStep(currentStep + 1)}
+                    >
+                      Next
+                      <ArrowRight size={12} />
+                    </button>
+                  )}
+                </div>
               </div>
             </RadixTooltip.Content>
           </RadixTooltip.Portal>
