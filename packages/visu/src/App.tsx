01e6dc4c4213c59257595c2ec4a8b7773f74e8bb
YaGRRusso <yagrrusso@gmail.com>
2023-04-05 17:59:31 -0300

    checkbox

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,7 +1,11 @@
-import { Avatar, Flex } from '@library'
+import { Flex } from '@library'
 import { clsx } from 'clsx'
+import { Check } from 'phosphor-react'
+import { useState } from 'react'
 
 function App() {
+  const [checked, setChecked] = useState(true)
+
   return (
     <Flex.Root center className="min-h-screen bg-gray-800">
       <Flex.Col
@@ -19,12 +23,22 @@ function App() {
         ])}
       >
         {/* ========================= TEST AREA ========================= */}
-        <Avatar
-          size="lg"
-          url="https://picsum.photos/200/200"
-          fallback="CO"
-          color="secondary"
-        />
+        <label className="flex items-center gap-2">
+          <div
+            className={clsx(
+              'cursor-pointer border-2 text-gray-100 w-4 h-4 transition-all flex items-center justify-center rounded-sm overflow-hidden',
+              {
+                'bg-primary border-primary': checked,
+                'border-gray hover:border-primary active:border-primary active:bg-primary':
+                  !checked,
+              }
+            )}
+            onClick={() => setChecked(!checked)}
+          >
+            {checked && <Check />}
+          </div>
+          <span>Opa</span>
+        </label>
         {/* ========================= TEST AREA ========================= */}
       </Flex.Col>
     </Flex.Root>
