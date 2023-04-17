a80d8c7e9ffaa8b50da580fa2db8f5efec717a15
Leandro Pedroso <lpedroso@meudroz.com>
2023-04-17 16:46:48 -0300

    Adicionado diretório types para organizar tipagens dos componentes

---
+++ packages/visu/src/library/types/Checkbox.ts
@@ -0,0 +1,6 @@
+export type CheckboxColor = keyof CheckboxColorClass
+
+export type CheckboxColorClass = {
+  primary: string
+  secondary: string
+}
---
+++ packages/visu/src/library/types/index.ts
@@ -0,0 +1,9 @@
+export * from './Checkbox'
+
+export type Size = keyof SizeClass
+
+export type SizeClass = {
+  sm: string
+  md: string
+  lg: string
+}
