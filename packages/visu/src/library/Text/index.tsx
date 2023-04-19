b743f460ffdf09f2ec31aa86bf08673c6a932ddc
Leandro Pedroso <lpedroso@meudroz.com>
2023-04-19 10:41:00 -0300

    Reestruturação componente Text

--- packages/visu/src/library/Text/index.tsx
+++ packages/visu/src/library/Text/index.tsx
@@ -1,20 +1,69 @@
 import { Slot } from '@radix-ui/react-slot'
+import {
+  Align,
+  AlignClass,
+  FullColor,
+  FullColorClass,
+  FullSize,
+  FullSizeClass,
+  Weight,
+  WeightClass,
+} from '@types'
 import { clsx } from 'clsx'
-import { HTMLAttributes, useMemo } from 'react'
+import { FC, HTMLAttributes, useMemo } from 'react'
 
 export interface TextProps extends HTMLAttributes<HTMLSpanElement> {
-  align?: 'left' | 'center' | 'right'
+  align?: Align
   asChild?: boolean
-  color?: 'primary' | 'secondary' | 'success' | 'error' | 'alert'
+  color?: FullColor
   ellipsis?: boolean
   inline?: boolean
-  link?: 'primary' | 'secondary' | 'success' | 'error' | 'alert'
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl'
+  link?: FullColor
+  size?: FullSize
   underline?: boolean
-  weight?: 'light' | 'normal' | 'semibold' | 'bold'
+  weight?: Weight
 }
 
-const Text = ({
+const alignClassVariants: AlignClass = {
+  center: 'text-center',
+  left: 'text-left',
+  right: 'text-right',
+}
+
+const colorClassVariants: FullColorClass = {
+  alert: 'text-alert',
+  error: 'text-error',
+  primary: 'text-primary',
+  secondary: 'text-secondary',
+  success: 'text-success',
+}
+
+const linkClassVariants: FullColorClass = {
+  alert: 'hover:text-alert-500 active:text-alert-600',
+  error: 'hover:text-error-500 active:text-error-600',
+  primary: 'hover:text-primary-500 active:text-primary-600',
+  secondary: 'hover:text-secondary-500 active:text-secondary-600',
+  success: 'hover:text-success-500 active:text-success-600',
+}
+
+const sizeClassVariants: FullSizeClass = {
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  lg: 'text-lg',
+  md: 'text-md',
+  sm: 'text-sm',
+  xl: 'text-xl',
+  xs: 'text-xs',
+}
+
+const weightClassVariants: WeightClass = {
+  bold: 'font-bold',
+  normal: 'font-normal',
+  light: 'font-light',
+  semibold: 'font-semibold',
+}
+
+const Text: FC<TextProps> = ({
   align,
   asChild,
   children,
@@ -27,87 +76,54 @@ const Text = ({
   underline,
   weight,
   ...rest
-}: TextProps) => {
+}) => {
   const Component = asChild ? Slot : 'span'
 
-  const alignClass = useMemo(() => {
-    return align
-      ? clsx({
-          'text-left': align === 'left',
-          'text-center': align === 'center',
-          'text-right': align === 'right',
-        })
-      : ''
+  const alignClass = useMemo<string>(() => {
+    return align ? alignClassVariants[align] : ''
   }, [align])
 
-  const colorClass = useMemo(() => {
-    return color
-      ? clsx({
-          'text-primary': color === 'primary',
-          'text-secondary': color === 'secondary',
-          'text-success': color === 'success',
-          'text-error': color === 'error',
-          'text-alert': color === 'alert',
-        })
-      : ''
+  const colorClass = useMemo<string>(() => {
+    return color ? colorClassVariants[color] : ''
   }, [color])
 
-  const linkClass = useMemo(() => {
+  const linkClass = useMemo<string>(() => {
     return link
-      ? clsx('cursor-pointer rounded-lg', {
-          'hover:text-primary-500 active:text-primary-600': link === 'primary',
-          'hover:text-secondary-500 active:text-secondary-600':
-            link === 'secondary',
-          'hover:text-success-500 active:text-success-600': link === 'success',
-          'hover:text-error-500 active:text-error-600': link === 'error',
-          'hover:text-alert-500 active:text-alert-600': link === 'alert',
-        })
+      ? ['cursor-pointer rounded-lg', linkClassVariants[link]].join(' ')
       : ''
   }, [link])
 
-  const ellipsisClass = useMemo(() => {
+  const ellipsisClass = useMemo<string>(() => {
     return ellipsis
       ? 'text-ellipsis overflow-hidden whitespace-nowrap max-w-full'
       : ''
   }, [ellipsis])
 
-  const inlineClass = useMemo(() => {
+  const inlineClass = useMemo<string>(() => {
     return inline ? 'inline-flex gap-px flex-wrap items-center' : ''
   }, [inline])
 
-  const sizeClass = useMemo(() => {
-    return size
-      ? clsx({
-          'text-xs': size === 'xs',
-          'text-sm': size === 'sm',
-          'text-md': size === 'md',
-          'text-lg': size === 'lg',
-          'text-xl': size === 'xl',
-          'text-2xl': size === '2xl',
-          'text-3xl': size === '3xl',
-        })
-      : ''
+  const sizeClass = useMemo<string>(() => {
+    return size ? sizeClassVariants[size] : ''
   }, [size])
 
-  const underlineClass = useMemo(() => {
+  const underlineClass = useMemo<string>(() => {
     return underline ? 'underline underline-offset-2' : ''
   }, [underline])
 
-  const weightClass = useMemo(() => {
-    return weight
-      ? clsx({
-          'font-light': weight === 'light',
-          'font-normal': weight === 'normal',
-          'font-semibold': weight === 'semibold',
-          'font-bold': weight === 'bold',
-        })
-      : ''
+  const weightClass = useMemo<string>(() => {
+    return weight ? weightClassVariants[weight] : ''
   }, [weight])
 
   return (
     <Component
       className={clsx([
         className,
+        'text-inherit-size',
+        'transition-all',
+        'children:text-inherit-size',
+        'children:inline',
+        '[&_svg]:align-middle',
         alignClass,
         colorClass,
         ellipsisClass,
@@ -116,11 +132,6 @@ const Text = ({
         sizeClass,
         underlineClass,
         weightClass,
-        'text-inherit-size',
-        'transition-all',
-        'children:text-inherit-size',
-        'children:inline',
-        '[&_svg]:align-middle',
       ])}
       {...rest}
     >
--- packages/visu/src/library/types/index.ts
+++ packages/visu/src/library/types/index.ts
@@ -2,6 +2,14 @@ export * from './Button'
 export * from './Flex'
 export * from './FloatCard'
 
+export type Align = keyof AlignClass
+
+export interface AlignClass {
+  center: string
+  left: string
+  right: string
+}
+
 export type Color = keyof ColorClass
 
 export interface ColorClass {
@@ -11,13 +19,27 @@ export interface ColorClass {
 
 export type ExtendedColor = keyof ExtendedColorClass
 
-export interface ExtendedColorClass extends ColorClass {
-  alert: string
+export interface ExtendedColorClass extends FullColorClass {
   current: string
-  error: string
   gray: string
-  success: string
   white: string
+}
+
+export type FullColor = keyof FullColorClass
+
+export interface FullColorClass extends ColorClass {
+  alert: string
+  error: string
+  success: string
+}
+
+export type FullSize = keyof FullSizeClass
+
+export interface FullSizeClass extends SizeClass {
+  '2xl': string
+  '3xl': string
+  xl: string
+  xs: string
 }
 
 export type LargerSizes = keyof LargerSizesClass
@@ -48,4 +70,13 @@ export type Status = keyof StatusClass
 export interface StatusClass {
   error: string
   success: string
+}
+
+export type Weight = keyof WeightClass
+
+export interface WeightClass {
+  bold: string
+  light: string
+  normal: string
+  semibold: string
 }
