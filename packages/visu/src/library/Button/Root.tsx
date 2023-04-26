7d27a65647235de4679f3ba4fe8040aba05ba39d
Leandro Pedroso <lpedroso@meudroz.com>
2023-04-26 11:44:09 -0300

    Modificada a implementação do Button Root

--- packages/visu/src/library/Button/Root.tsx
+++ packages/visu/src/library/Button/Root.tsx
@@ -5,9 +5,11 @@ import {
   ButtonHTMLAttributes,
   Ref,
   forwardRef,
+  useEffect,
   useImperativeHandle,
   useMemo,
   useRef,
+  useState,
 } from 'react'
 
 import LoadingDots from '../Loading'
@@ -23,6 +25,19 @@ export interface ButtonRootProps
   variant?: ButtonVariant
 }
 
+const ghostClassVariants: ButtonVariantClass = {
+  neutral: 'border border-solid bg-transparent border-gray-600 text-gray-900',
+  primary: 'border border-solid bg-transparent border-primary text-primary',
+  secondary:
+    'border border-solid bg-transparent border-secondary text-secondary',
+}
+
+const lightClassVariants: ButtonVariantClass = {
+  neutral: 'bg-transparent text-gray-900',
+  primary: 'bg-transparent text-primary',
+  secondary: 'bg-transparent text-secondary',
+}
+
 const sizeClassVariants: SizeClass = {
   sm: 'py-2.5 px-6 text-sm min-h-10',
   md: 'py-3 px-10 min-h-12',
@@ -31,11 +46,11 @@ const sizeClassVariants: SizeClass = {
 
 const variantClassVariants: ButtonVariantClass = {
   neutral:
-    'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-200 active:bg-gray-300',
+    'bg-gray-100 border-none text-gray-900 shadow-sm hover:bg-gray-200 active:bg-gray-300',
   primary:
-    'bg-primary text-gray-100 hover:bg-primary-500 active:bg-primary-600',
+    'bg-primary border-none text-gray-100 hover:bg-primary-500 active:bg-primary-600',
   secondary:
-    'bg-secondary text-gray-100 hover:bg-secondary-500 active:bg-secondary-600',
+    'bg-secondary border-none text-gray-100 hover:bg-secondary-500 active:bg-secondary-600',
 }
 
 const ButtonRoot = forwardRef<HTMLButtonElement, ButtonRootProps>(
@@ -60,20 +75,25 @@ const ButtonRoot = forwardRef<HTMLButtonElement, ButtonRootProps>(
 
     useImperativeHandle(ref, () => buttonRef.current)
 
+    const [isDisabled, setIsDisabled] = useState<boolean | undefined>(disabled)
+
     // Variants
 
-    const disabledClass = useMemo<string>(() => {
-      return disabled || loading
-        ? clsx({
-            'pointer-events-none': disabled || loading,
-            '!bg-gray !text-gray-500':
-              (disabled || loading) && !ghost && !light,
-            '!border-gray !text-gray-500 !bg-transparent':
-              (disabled || loading) && ghost,
-            'text-gray bg-transparent': (disabled || loading) && light,
-          })
-        : ''
-    }, [disabled, loading, ghost, light, variant])
+    useEffect(() => {
+      setIsDisabled(disabled || loading)
+    }, [disabled, loading])
+
+    const rootClass = useMemo(() => {
+      if (isDisabled) {
+        if (ghost)
+          return 'pointer-events-none border-gray text-gray-500 bg-transparent'
+        if (light) return 'pointer-events-none text-gray bg-transparent'
+        return 'pointer-events-none bg-gray text-gray-500'
+      }
+      if (light) return lightClassVariants[variant]
+      if (ghost) return ghostClassVariants[variant]
+      return variantClassVariants[variant]
+    }, [ghost, isDisabled, light, variant])
 
     const fullClass = useMemo<string>(() => {
       return full ? '!w-full' : 'w-fit'
@@ -83,41 +103,9 @@ const ButtonRoot = forwardRef<HTMLButtonElement, ButtonRootProps>(
       return loading ? 'opacity-0' : ''
     }, [loading])
 
-    const ghostClass = useMemo<string>(() => {
-      return ghost ? '!border !border-solid bg-transparent' : ''
-    }, [ghost])
-
-    const lightClass = useMemo<string>(() => {
-      return light ? 'border-none !bg-transparent !p-3' : ''
-    }, [light])
-
     const sizeClass = useMemo<string>(() => {
       return sizeClassVariants[size]
     }, [size])
-
-    const variantClass = useMemo<string>(() => {
-      return variantClassVariants[variant]
-    }, [variant])
-
-    // Compound Variants
-
-    const compoundVariantsClass = useMemo<string>(() => {
-      const ghostAndVariantClass = clsx({
-        'border-primary text-primary hover:!border-primary-500 hover:!text-primary-500 hover:!bg-primary-100 active:!border-primary-600 active:!text-primary-600 active:!bg-primary-100':
-          ghost && variant === 'primary',
-        'border-secondary text-secondary hover:!border-secondary-500 hover:!text-secondary-500 hover:!bg-secondary-100 active:!border-secondary-600 active:!text-secondary-600 active:!bg-secondary-100':
-          ghost && variant === 'secondary',
-      })
-
-      const lightAndVariantClass = clsx({
-        'text-primary hover:text-primary-500 !hover:bg-transparent active:text-primary-600 !active:bg-transparent':
-          light && variant === 'primary',
-        'text-secondary hover:text-secondary-500 !hover:bg-transparent active:text-secondary-600 !active:bg-transparent':
-          light && variant === 'secondary',
-      })
-
-      return clsx(ghostAndVariantClass, lightAndVariantClass)
-    }, [ghost, light, variant])
 
     return (
       <RootComponent
@@ -130,18 +118,13 @@ const ButtonRoot = forwardRef<HTMLButtonElement, ButtonRootProps>(
           'justify-center',
           'relative',
           'rounded-lg',
-          'border-none',
           'cursor-pointer',
           'transition-all',
-          compoundVariantsClass,
           fullClass,
-          ghostClass,
-          lightClass,
+          rootClass,
           sizeClass,
-          variantClass,
-          disabledClass,
         ])}
-        disabled={disabled || loading}
+        disabled={isDisabled}
         ref={buttonRef}
         {...rest}
       >
