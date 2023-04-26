206e44a09840e8c40348e48302a3893e222335a6
Leandro Pedroso <lpedroso@meudroz.com>
2023-04-26 12:16:22 -0300

    Adicionado forwardRef ao Checkbox.Root

--- packages/visu/src/library/Checkbox/Root.tsx
+++ packages/visu/src/library/Checkbox/Root.tsx
@@ -1,7 +1,7 @@
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import { Color, ColorClass } from '@types'
 import { clsx } from 'clsx'
-import { FC, useMemo } from 'react'
+import { Ref, forwardRef, useImperativeHandle, useMemo, useRef } from 'react'
 
 export interface CheckboxRootProps extends RadixCheckbox.CheckboxProps {
   color?: Color
@@ -14,47 +14,51 @@ export const rootColorVariants: ColorClass = {
     'data-[state=checked]:bg-secondary data-[state=checked]:border-secondary border-gray hover:border-secondary active:border-secondary active:bg-secondary',
 }
 
-const CheckboxRoot: FC<CheckboxRootProps> = ({
-  children,
-  className,
-  color = 'primary',
-  disabled,
-  ...rest
-}) => {
-  const colorClass = useMemo(() => {
-    return rootColorVariants[color]
-  }, [color])
+const CheckboxRoot = forwardRef<HTMLButtonElement, CheckboxRootProps>(
+  (
+    { children, className, color = 'primary', disabled, ...rest },
+    ref: Ref<HTMLButtonElement | null>
+  ) => {
+    const checkboxRef = useRef<HTMLButtonElement>(null)
 
-  const disabledClass = useMemo(() => {
-    return disabled
-      ? 'data-[state=checked]:!bg-gray-400 data-[state=checked]:!border-gray !bg-gray-300 !border-gray'
-      : ''
-  }, [disabled])
+    useImperativeHandle(ref, () => checkboxRef.current)
 
-  return (
-    <RadixCheckbox.Root
-      className={clsx([
-        className,
-        'border-2',
-        'text-gray-100',
-        'w-4',
-        'h-4',
-        'transition-all',
-        'flex',
-        'items-center',
-        'justify-center',
-        'rounded-sm',
-        'overflow-hidden',
-        colorClass,
-        disabledClass,
-      ])}
-      disabled={disabled}
-      {...rest}
-    >
-      {children}
-    </RadixCheckbox.Root>
-  )
-}
+    const colorClass = useMemo(() => {
+      return rootColorVariants[color]
+    }, [color])
+
+    const disabledClass = useMemo(() => {
+      return disabled
+        ? 'data-[state=checked]:!bg-gray-400 data-[state=checked]:!border-gray !bg-gray-300 !border-gray'
+        : ''
+    }, [disabled])
+
+    return (
+      <RadixCheckbox.Root
+        className={clsx([
+          className,
+          'border-2',
+          'text-gray-100',
+          'w-4',
+          'h-4',
+          'transition-all',
+          'flex',
+          'items-center',
+          'justify-center',
+          'rounded-sm',
+          'overflow-hidden',
+          colorClass,
+          disabledClass,
+        ])}
+        disabled={disabled}
+        ref={checkboxRef}
+        {...rest}
+      >
+        {children}
+      </RadixCheckbox.Root>
+    )
+  }
+)
 
 CheckboxRoot.displayName = 'Checkbox.Root'
 
