85678fc8b8953ff3715d1ff3189a5c66fc950c57
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-11 19:30:05 -0300

    Atualizada documentação do componente Checkbox

---
+++ apps/docs/src/stories/Checkbox/Indicator.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Question } from 'phosphor-react'
+import { withDesign } from 'storybook-addon-designs'
+
+import { Checkbox, CheckboxIndicatorProps } from '@coaktion/visu'
+
+const meta: Meta<CheckboxIndicatorProps> = {
+  title: 'Checkbox/Indicator',
+  component: Checkbox.Indicator,
+  decorators: [withDesign],
+  argTypes: {
+    children: {
+      control: 'none',
+      description:
+        'Recebe um ícone como filho para modificar o ícone padrão. Caso nenhum filho seja passado, o ícone padrão é utilizado. É necessário passar o ícone para os casos de checkbox com estado indeterminado.',
+      table: {
+        type: {
+          summary: 'React.ReactNode',
+        },
+      },
+    },
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cUmiZr1GhrB9HsRCuOJ0S2/%5BDroz-Nexo%5D-Library?type=design&node-id=3107-18146&t=i2mg4ixu1iYnYf6w-0',
+    },
+  },
+}
+
+export default meta
+type CheckboxRootStory = StoryObj<CheckboxIndicatorProps>
+
+export const Comum: CheckboxRootStory = {
+  render: (args) => {
+    return (
+      <Checkbox.Root checked>
+        <Checkbox.Indicator {...args} />
+      </Checkbox.Root>
+    )
+  },
+}
+
+export const Indeterminado: CheckboxRootStory = {
+  render: (args) => {
+    return (
+      <Checkbox.Root checked="indeterminate">
+        <Checkbox.Indicator {...args}>
+          <Question />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+    )
+  },
+}
---
+++ apps/docs/src/stories/Checkbox/Root.stories.tsx
@@ -0,0 +1,91 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Check, Question } from 'phosphor-react'
+import { withDesign } from 'storybook-addon-designs'
+
+import { Checkbox, CheckboxRootProps } from '@coaktion/visu'
+
+const meta: Meta<CheckboxRootProps> = {
+  title: 'Checkbox/Root',
+  component: Checkbox.Root,
+  decorators: [withDesign],
+  argTypes: {
+    children: {
+      control: 'none',
+      table: {
+        type: {
+          summary: 'React.ReactNode',
+        },
+      },
+    },
+    checked: {
+      control: { type: 'inline-radio' },
+      description:
+        'Propriedade que indica se o componente está marcado, não está marcado ou está indeterminado.',
+      options: [true, false, 'indeterminate'],
+      table: {
+        type: { summary: ['boolean', '"indeterminate"'].join(' | ') },
+        defaultValue: { summary: false },
+      },
+      type: { name: 'boolean', required: false },
+    },
+    color: {
+      control: 'inline-radio',
+      description: 'Define a cor do componente.',
+      options: ['primary', 'secondary'] as CheckboxRootProps['color'][],
+      table: {
+        type: {
+          summary: ['primary', 'secondary'].join('|'),
+        },
+        defaultValue: { summary: 'primary' },
+      },
+      type: { name: 'string', required: false },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      description: 'Desabilita o componente.',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      type: { name: 'boolean', required: false },
+    },
+    onCheckedChange: {
+      action: 'clicked',
+      description: 'Evento de modificação do valor do componente.',
+      table: { type: { summary: 'function' } },
+    },
+  },
+  args: {
+    children: '',
+    checked: false,
+    color: 'primary',
+    disabled: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cUmiZr1GhrB9HsRCuOJ0S2/%5BDroz-Nexo%5D-Library?type=design&node-id=3107-18146&t=i2mg4ixu1iYnYf6w-0',
+    },
+  },
+}
+
+export default meta
+type CheckboxRootStory = StoryObj<CheckboxRootProps>
+
+export const Comum: CheckboxRootStory = {
+  render: (args) => {
+    return <Checkbox.Root {...args} />
+  },
+}
+
+export const Indeterminado: CheckboxRootStory = {
+  render: (args) => {
+    return (
+      <Checkbox.Root {...args}>
+        <Checkbox.Indicator>
+          {args.checked === 'indeterminate' ? <Question /> : <Check />}
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+    )
+  },
+}
