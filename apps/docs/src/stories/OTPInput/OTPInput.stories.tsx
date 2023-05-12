337e18a048dfeaa4161eec964af235ad84801f49
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-12 20:51:09 -0300

    Atualizada documentação do componente OTPInput

--- apps/docs/src/stories/OTPInput/OTPInput.stories.tsx
+++ apps/docs/src/stories/OTPInput/OTPInput.stories.tsx
@@ -1,41 +1,72 @@
-import { ComponentStory, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 import { OTPInput, OTPInputProps } from '@coaktion/visu'
 
-export default {
+const meta: Meta<OTPInputProps> = {
   title: 'OTPInput/OTPInput',
   component: OTPInput,
   argTypes: {
-    children: {
+    count: {
+      control: 'number',
+      description:
+        'Define a quantidade de entradas do componente. <b>É necessário recarregar a página para ver a modificação</b>.',
       table: {
-        type: {
-          summary: 'React.ReactNode',
-        },
+        type: { summary: 'number' },
+        defaultValue: { summary: 1 },
       },
+      type: { name: 'number', required: true },
     },
     error: {
       control: 'boolean',
-      description: 'Aplica o error em OTPInputOTPInput',
+      description: 'Propriedade que aplica um estado de erro ao componente.',
       table: {
         type: { summary: 'boolean' },
       },
       type: { name: 'boolean', required: false },
     },
-    count: {
-      control: 'number',
-      description: 'Aplica o count em OTPInputOTPInput',
+    value: {
+      control: 'none',
+      description:
+        'Propriedade que armazena o valor atual do componente. <b>Exemplo: Se estiver exibindo 1 2 3 4 5, o valor atual será ["1", "2", "3", "4", "5"]</b>.',
       table: {
-        type: { summary: 'number' },
+        type: { summary: 'string[]' },
+        defaultValue: { summary: '[""]' },
       },
-      type: { name: 'number', required: true },
-      defaultValue: 5 as OTPInputProps['count'],
+    },
+    onChange: {
+      action: 'changed',
+      description: 'Evento de modificação do valor do componente.',
+      table: { type: { summary: 'function' } },
     },
   },
   args: {
-    children: '',
+    count: 1,
+    error: false,
+    value: [],
   },
-} as Meta<OTPInputProps>
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cUmiZr1GhrB9HsRCuOJ0S2/%5BDroz-Nexo%5D-Library?type=design&node-id=3426-863&t=AdZjH6UtpERmhB5O-0',
+      allowFullscreen: true,
+    },
+  },
+}
 
-export const Comum: ComponentStory<typeof OTPInput> = (args) => {
-  return <OTPInput {...args} />
+export default meta
+type OTPInputStory = StoryObj<OTPInputProps>
+
+export const Comum: OTPInputStory = {
+  render: (args) => {
+    return <OTPInput {...args} />
+  },
+}
+
+export const MaisDigitos: OTPInputStory = {
+  args: {
+    count: 5,
+  },
+  render: (args) => {
+    return <OTPInput {...args} />
+  },
 }
