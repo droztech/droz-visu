6cbcc432c4daab395ff9a06db90d9fdc8a413cad
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-12 20:30:17 -0300

    Atualizada documentação do componente Menu

--- apps/docs/src/stories/Menu/Divider.stories.tsx
+++ apps/docs/src/stories/Menu/Divider.stories.tsx
@@ -1,12 +1,13 @@
-import { ComponentStory, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 import { Menu, MenuDividerProps } from '@coaktion/visu'
 
-export default {
+const meta: Meta<MenuDividerProps> = {
   title: 'Menu/Divider',
   component: Menu.Divider,
   argTypes: {
     children: {
+      control: 'none',
       table: {
         type: {
           summary: 'React.ReactNode',
@@ -17,14 +18,82 @@ export default {
   args: {
     children: '',
   },
-} as Meta<MenuDividerProps>
-
-const Template: ComponentStory<typeof Menu.Divider> = (args) => {
-  return (
-    <div style={{ width: '240px' }}>
-      <Menu.Divider {...args}></Menu.Divider>
-    </div>
-  )
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/vimM8ueWbwKbUB2F4anWLS/%5BDroz-Nexo%5D-Telas?type=design&node-id=3295-23409&t=L7rzcV7ijjLp260X-0',
+      allowFullscreen: true,
+    },
+  },
 }
 
-export const Comum = Template.bind({})
+export default meta
+type MenuDividerStory = StoryObj<MenuDividerProps>
+
+export const EntreGrupos: MenuDividerStory = {
+  render: (args) => {
+    return (
+      <Menu.Root
+        expanded
+        style={{
+          position: 'unset',
+          width: '300px',
+          border: '1px dashed red',
+        }}
+      >
+        Menu
+        <Menu.Group
+          {...args}
+          style={{
+            width: '100%',
+            height: '60px',
+            border: '1px dashed green',
+          }}
+        >
+          Grupo 1
+        </Menu.Group>
+        <Menu.Divider {...args} />
+        <Menu.Group
+          {...args}
+          style={{
+            width: '100%',
+            height: '60px',
+            border: '1px dashed blue',
+          }}
+        >
+          Grupo 2
+        </Menu.Group>
+      </Menu.Root>
+    )
+  },
+}
+
+export const EntreElementos: MenuDividerStory = {
+  name: 'Entre elementos do grupo',
+  render: (args) => {
+    return (
+      <Menu.Root
+        expanded
+        style={{
+          position: 'unset',
+          width: '300px',
+          border: '1px dashed red',
+        }}
+      >
+        Menu
+        <Menu.Group
+          {...args}
+          style={{
+            width: '100%',
+            border: '1px dashed green',
+          }}
+        >
+          <span>Grupo 1</span>
+          <span>Elemento 1</span>
+          <Menu.Divider {...args} />
+          <span>Elemento 2</span>
+        </Menu.Group>
+      </Menu.Root>
+    )
+  },
+}
