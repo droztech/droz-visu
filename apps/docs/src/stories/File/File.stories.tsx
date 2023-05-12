e39bfbc366b92465a672ecfb4bcdd443413580f5
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-12 09:55:50 -0300

    Atualizada documentação do componente File

--- apps/docs/src/stories/File/File.stories.tsx
+++ apps/docs/src/stories/File/File.stories.tsx
@@ -1,10 +1,12 @@
-import { ComponentStory, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
+import { withDesign } from 'storybook-addon-designs'
 
 import { File, FileProps } from '@coaktion/visu'
 
-export default {
+const meta: Meta<FileProps> = {
   title: 'File/File',
   component: File,
+  decorators: [withDesign],
   argTypes: {
     formats: {
       control: 'object',
@@ -19,8 +21,19 @@ export default {
   args: {
     children: '',
   },
-} as Meta<FileProps>
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/vimM8ueWbwKbUB2F4anWLS/%5BDroz-Nexo%5D-Telas?type=design&node-id=3295-27317&t=t0RzfGnHc1izbdAY-0',
+    },
+  },
+}
 
-export const Comum: ComponentStory<typeof File> = (args) => {
-  return <File {...args}></File>
+export default meta
+type FileStory = StoryObj<FileProps>
+
+export const Comum: FileStory = {
+  render: (args) => {
+    return <File {...args}></File>
+  },
 }
