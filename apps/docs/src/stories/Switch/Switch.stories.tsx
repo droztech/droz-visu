3bafaa23fddb55254e4306b5c47796bb6d621aa7
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-15 18:15:27 -0300

    Atualizada documentação do componente Switch

--- apps/docs/src/stories/Switch/Switch.stories.tsx
+++ apps/docs/src/stories/Switch/Switch.stories.tsx
@@ -1,53 +1,79 @@
-import { useArgs } from '@storybook/client-api'
-import { ComponentStory, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 import { Switch, SwitchProps } from '@coaktion/visu'
 
-export default {
+const meta: Meta<SwitchProps> = {
   title: 'Switch/Switch',
   component: Switch,
   argTypes: {
-    children: {
-      table: {
-        type: {
-          summary: 'React.ReactNode',
-        },
-      },
-    },
-    disabled: {
-      control: 'boolean',
-      description: 'Aplica o disabled em Switch',
+    asChild: {
+      control: { type: 'boolean' },
+      description:
+        'Ao passar essa propriedade com o valor true, o componente irá se transformar no componente filho. É obrigatório passar um children ao utilizar essa propriedade. <b>Verifique a tag ao inspecionar o componente na DOM</b>.',
       table: {
         type: { summary: 'boolean' },
+        defaultValue: { summary: false },
       },
       type: { name: 'boolean', required: false },
     },
     checked: {
       control: 'boolean',
-      description: 'Aplica o checked em Switch',
+      description: 'Propriedade que define se o valor está verificado.',
       table: {
         type: { summary: 'boolean' },
       },
       type: { name: 'boolean', required: false },
     },
+    defaultChecked: {
+      control: 'boolean',
+      description: 'Define o valor inicial do componente.',
+      table: {
+        type: { summary: 'boolean' },
+      },
+      type: { name: 'boolean', required: false },
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Desabilita o componente.',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      type: { name: 'boolean', required: false },
+    },
+    required: {
+      control: { type: 'boolean' },
+      description:
+        'Define se é obrigatório verificar antes de submeter em um formulário.',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      type: { name: 'boolean', required: false },
+    },
     onCheckedChange: {
-      action: 'clicked',
-      description: 'Define o onCheckedChange em Switch',
+      action: 'change',
+      description: 'Evento de mudança de valor.',
       table: { type: { summary: 'function' } },
     },
   },
   args: {
-    children: '',
+    asChild: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cUmiZr1GhrB9HsRCuOJ0S2/%5BDroz-Nexo%5D-Library?type=design&node-id=3107-18282&t=F4jFiS042bjLT7s3-0',
+      allowFullscreen: true,
+    },
   },
 } as Meta<SwitchProps>
 
-export const Comum: ComponentStory<typeof Switch> = (args) => {
-  const [{ checked }, updateArgs] = useArgs()
-  delete args.onCheckedChange
+export default meta
+type SwitchStory = StoryObj<SwitchProps>
 
-  const toggleChecked = () => {
-    updateArgs({ checked: !checked })
-  }
-
-  return <Switch onCheckedChange={toggleChecked} {...args}></Switch>
+export const Comum: SwitchStory = {
+  render: (args) => {
+    return <Switch {...args}></Switch>
+  },
 }
