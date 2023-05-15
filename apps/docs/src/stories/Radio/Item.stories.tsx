63c55e1d3268a625bbd816cb68b475df515a3257
Leandro Pedroso <lpedroso@meudroz.com>
2023-05-15 11:51:39 -0300

    Atualizada documentação do componente Radio

--- apps/docs/src/stories/Radio/Item.stories.tsx
+++ apps/docs/src/stories/Radio/Item.stories.tsx
@@ -1,18 +1,20 @@
-import { ComponentStory, Meta } from '@storybook/react'
-import { useState } from 'react'
+import { Meta, StoryObj } from '@storybook/react'
 
-import { Radio, RadioItemProps } from '@coaktion/visu'
+import { Flex, Radio, RadioItemProps } from '@coaktion/visu'
 
-export default {
+const meta: Meta<RadioItemProps> = {
   title: 'Radio/Item',
   component: Radio.Item,
   argTypes: {
-    children: {
+    asChild: {
+      control: { type: 'boolean' },
+      description:
+        'Ao passar essa propriedade com o valor true, o componente irá se transformar no componente filho. É obrigatório passar um children ao utilizar essa propriedade. <b>Verifique a tag ao inspecionar o componente na DOM</b>.',
       table: {
-        type: {
-          summary: 'React.ReactNode',
-        },
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
       },
+      type: { name: 'boolean', required: false },
     },
     color: {
       control: 'inline-radio',
@@ -22,9 +24,9 @@ export default {
         type: {
           summary: ['primary', 'secondary'].join('|'),
         },
+        defaultValue: { summary: 'primary' },
       },
       type: { name: 'string', required: false },
-      defaultValue: 'primary' as RadioItemProps['color'],
     },
     disabled: {
       control: 'boolean',
@@ -34,26 +36,57 @@ export default {
       },
       type: { name: 'boolean', required: false },
     },
+    required: {
+      control: { type: 'boolean' },
+      description:
+        'Define se é obrigatório marcar o item antes de submeter em um formulário.',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      type: { name: 'boolean', required: false },
+    },
+    value: {
+      control: 'text',
+      description: 'Define o valor do item.',
+      table: {
+        type: { summary: 'text' },
+        defaultValue: { summary: '' },
+      },
+      type: { name: 'string', required: true },
+    },
   },
   args: {
-    children: '',
+    asChild: false,
+    color: 'primary',
+    disabled: false,
+    required: false,
   },
-} as Meta<RadioItemProps>
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cUmiZr1GhrB9HsRCuOJ0S2/%5BDroz-Nexo%5D-Library?type=design&node-id=3107-18214&t=gSZqfhVYy3KRKNHz-0',
+      allowFullscreen: true,
+    },
+  },
+}
 
-export const Comum: ComponentStory<typeof Radio.Item> = (args) => {
-  const [value, setValue] = useState('yes')
+export default meta
+type RadioItemStory = StoryObj<RadioItemProps>
 
-  return (
-    <Radio.Root value={value} onValueChange={(ev) => setValue(ev)}>
-      <Radio.Item {...args} value="yes">
-        Yes
-      </Radio.Item>
-      <Radio.Item {...args} value="no">
-        No
-      </Radio.Item>
-      <Radio.Item {...args} value="maybe">
-        Maybe
-      </Radio.Item>
-    </Radio.Root>
-  )
+export const Comum: RadioItemStory = {
+  render: ({ value, ...args }) => {
+    return (
+      <Radio.Root>
+        <Flex.Row center>
+          <Radio.Item id="item1" value="1" {...args} />
+          <span>Item 1</span>
+        </Flex.Row>
+        <Flex.Row center>
+          <Radio.Item id="item2" value="2" {...args} />
+          <span>Item 2</span>
+        </Flex.Row>
+      </Radio.Root>
+    )
+  },
 }
