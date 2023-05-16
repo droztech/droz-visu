8b19733bda2813c59499e0e042446f1c945c1a9e
YaGRRusso <yagrrusso@gmail.com>
2023-05-16 14:16:18 -0300

    layout hooks

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,13 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import {
-  DeviceMobileCamera,
-  DeviceTabletCamera,
-  Monitor,
-  Moon,
-  Sun,
-  User,
-} from 'phosphor-react'
-import { useState } from 'react'
+import { Question } from 'phosphor-react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -21,21 +13,10 @@ const formSchema = z.object({
 type FormSchemaProps = z.infer<typeof formSchema>
 
 function App() {
-  const [screen, setScreen] = useState<string>()
-  const [theme, setTheme] = useState<'light' | 'dark'>(
-    (localStorage.getItem('visuDevTheme') as 'light' | 'dark') ?? 'light'
-  )
-
   const { handleSubmit, watch, register } = useForm<FormSchemaProps>({
     resolver: zodResolver(formSchema),
     defaultValues: { data: '' },
   })
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light'
-    setTheme(newTheme)
-    localStorage.setItem('visuDevTheme', newTheme)
-  }
 
   const onSubmit: SubmitHandler<FormSchemaProps> = (data) => {
     console.log(data)
@@ -45,21 +26,14 @@ function App() {
     <LayoutDefault
       asChild
       terminal={watch()}
-      theme={theme}
-      screenSize={screen}
       buttons={[
-        { icon: theme === 'dark' ? <Sun /> : <Moon />, onClick: toggleTheme },
-        { icon: <DeviceMobileCamera />, onClick: () => setScreen('375px') },
-        { icon: <DeviceTabletCamera />, onClick: () => setScreen('768px') },
-        { icon: <Monitor />, onClick: () => setScreen(undefined) },
+        { icon: <Question />, onClick: () => console.log('Place Anything') },
+        { icon: <Question />, onClick: () => console.log('Place Anything') },
       ]}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* ================================= TEST AREA ================================= */}
         <Input.Root>
-          <Input.Icon>
-            <User />
-          </Input.Icon>
           <Input.Input placeholder="Digite aqui..." {...register('data')} />
         </Input.Root>
         {/* ================================= TEST AREA ================================= */}
--- packages/visu/src/layout/Default.tsx
+++ packages/visu/src/layout/Default.tsx
@@ -1,12 +1,17 @@
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
-import { Terminal } from 'phosphor-react'
-import { FC, HTMLAttributes, ReactNode } from 'react'
+import {
+  DeviceMobileCamera,
+  DeviceTabletCamera,
+  Monitor,
+  Moon,
+  Sun,
+  Terminal,
+} from 'phosphor-react'
+import { FC, HTMLAttributes, ReactNode, useState } from 'react'
 
 export interface LayoutDefaultProps extends HTMLAttributes<HTMLDivElement> {
-  screenSize?: string
   asChild?: boolean
-  theme?: 'light' | 'dark'
   terminal?: object
   buttons?: {
     icon: ReactNode
@@ -15,8 +20,6 @@ export interface LayoutDefaultProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const LayoutDefault: FC<LayoutDefaultProps> = ({
-  theme = 'light',
-  screenSize,
   asChild,
   terminal,
   children,
@@ -26,20 +29,50 @@ const LayoutDefault: FC<LayoutDefaultProps> = ({
   ...rest
 }) => {
   const Comp = asChild ? Slot : 'div'
+  const [screen, setScreen] = useState<string>()
+  const [theme, setTheme] = useState<'light' | 'dark'>(
+    (localStorage.getItem('visuDevTheme') as 'light' | 'dark') ?? 'light'
+  )
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+    localStorage.setItem('visuDevTheme', newTheme)
+  }
+
+  const layoutButtons = [
+    { icon: theme === 'dark' ? <Sun /> : <Moon />, onClick: toggleTheme },
+    { icon: <DeviceMobileCamera />, onClick: () => setScreen('375px') },
+    { icon: <DeviceTabletCamera />, onClick: () => setScreen('768px') },
+    { icon: <Monitor />, onClick: () => setScreen(undefined) },
+  ]
 
   return (
     <div className="min-h-screen bg-[#272727] flex items-center justify-center">
       <div className="w-[90vw] min-w-[320px] max-w-5xl items-center justify-center flex-col gap-8 flex">
-        <div className="flex gap-4 items-center w-full">
-          {buttons?.map((item, index) => (
-            <button
-              key={index}
-              className="p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 text-gray-100"
-              onClick={item.onClick}
-            >
-              {item.icon}
-            </button>
-          ))}
+        <div className="flex gap-4 items-center w-full justify-between flex-wrap-reverse max-sm:justify-center">
+          <div className="flex gap-4 max-md:gap-2 items-center">
+            {buttons?.map((item, index) => (
+              <button
+                key={index}
+                className="p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 text-gray-100"
+                onClick={item.onClick}
+              >
+                {item.icon}
+              </button>
+            ))}
+          </div>
+          <div className="flex gap-4 max-md:gap-2 items-center">
+            {layoutButtons?.map((item, index) => (
+              <button
+                key={index}
+                className="p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 text-gray-100"
+                onClick={item.onClick}
+              >
+                {item.icon}
+              </button>
+            ))}
+          </div>
         </div>
         <Comp
           className={clsx(
@@ -50,7 +83,7 @@ const LayoutDefault: FC<LayoutDefaultProps> = ({
               : 'bg-gray-800 text-gray-100 dark'
           )}
           style={{
-            maxWidth: screenSize,
+            maxWidth: screen,
             ...style,
           }}
           {...rest}
@@ -58,7 +91,7 @@ const LayoutDefault: FC<LayoutDefaultProps> = ({
           {children}
         </Comp>
         {terminal && (
-          <pre className="bg-gray-800  p-4 rounded text-gray-400 relative min-h-12 text-xs w-full">
+          <pre className="bg-gray-800 p-4 rounded text-gray-400 relative min-h-12 text-xs w-full">
             {JSON.stringify(terminal, null, 2)}
             <button
               className="absolute top-2 right-2 p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 border border-gray-700"
