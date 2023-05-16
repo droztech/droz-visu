3ac277d2cc8389a356cf753ae2e5c358ab19540b
YaGRRusso <yagrrusso@gmail.com>
2023-05-16 13:45:04 -0300

    new app

--- packages/visu/src/App.tsx
+++ packages/visu/src/App.tsx
@@ -1,9 +1,18 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Terminal } from 'phosphor-react'
+import {
+  DeviceMobileCamera,
+  DeviceTabletCamera,
+  Monitor,
+  Moon,
+  Sun,
+  User,
+} from 'phosphor-react'
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import LayoutDefault from './layout/Default'
+import { Input } from './library'
 
 const formSchema = z.object({
   data: z.string().nonempty('required'),
@@ -12,10 +21,21 @@ const formSchema = z.object({
 type FormSchemaProps = z.infer<typeof formSchema>
 
 function App() {
-  const { handleSubmit, getValues } = useForm<FormSchemaProps>({
+  const [screen, setScreen] = useState<string>()
+  const [theme, setTheme] = useState<'light' | 'dark'>(
+    (localStorage.getItem('visuDevTheme') as 'light' | 'dark') ?? 'light'
+  )
+
+  const { handleSubmit, watch, register } = useForm<FormSchemaProps>({
     resolver: zodResolver(formSchema),
     defaultValues: { data: '' },
   })
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+    localStorage.setItem('visuDevTheme', newTheme)
+  }
 
   const onSubmit: SubmitHandler<FormSchemaProps> = (data) => {
     console.log(data)
@@ -24,13 +44,24 @@ function App() {
   return (
     <LayoutDefault
       asChild
+      terminal={watch()}
+      theme={theme}
+      screenSize={screen}
       buttons={[
-        { icon: <Terminal />, onClick: () => console.log(getValues()) },
+        { icon: theme === 'dark' ? <Sun /> : <Moon />, onClick: toggleTheme },
+        { icon: <DeviceMobileCamera />, onClick: () => setScreen('375px') },
+        { icon: <DeviceTabletCamera />, onClick: () => setScreen('768px') },
+        { icon: <Monitor />, onClick: () => setScreen(undefined) },
       ]}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* ================================= TEST AREA ================================= */}
-
+        <Input.Root>
+          <Input.Icon>
+            <User />
+          </Input.Icon>
+          <Input.Input placeholder="Digite aqui..." {...register('data')} />
+        </Input.Root>
         {/* ================================= TEST AREA ================================= */}
       </form>
     </LayoutDefault>
--- packages/visu/src/layout/Default.tsx
+++ packages/visu/src/layout/Default.tsx
@@ -1,10 +1,13 @@
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
-import { Moon, Sun } from 'phosphor-react'
-import { FC, HTMLAttributes, ReactNode, useState } from 'react'
+import { Terminal } from 'phosphor-react'
+import { FC, HTMLAttributes, ReactNode } from 'react'
 
 export interface LayoutDefaultProps extends HTMLAttributes<HTMLDivElement> {
+  screenSize?: string
   asChild?: boolean
+  theme?: 'light' | 'dark'
+  terminal?: object
   buttons?: {
     icon: ReactNode
     onClick: (data?: unknown) => void
@@ -12,38 +15,26 @@ export interface LayoutDefaultProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const LayoutDefault: FC<LayoutDefaultProps> = ({
+  theme = 'light',
+  screenSize,
   asChild,
+  terminal,
   children,
   className,
   buttons,
+  style,
   ...rest
 }) => {
   const Comp = asChild ? Slot : 'div'
 
-  const [theme, setTheme] = useState<'light' | 'dark'>(
-    (localStorage.getItem('visuDevTheme') as 'light' | 'dark') ?? 'light'
-  )
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light'
-    setTheme(newTheme)
-    localStorage.setItem('visuDevTheme', newTheme)
-  }
-
   return (
     <div className="min-h-screen bg-[#272727] flex items-center justify-center">
-      <div className="w-[90vw] min-w-[320px] max-w-5xl items-stretch justify-center flex-col gap-8 flex">
-        <div className="flex gap-4 items-center">
-          <button
-            className="p-2 rounded bg-gray-700 transition-colors hover:bg-gray-800 text-gray-100"
-            onClick={toggleTheme}
-          >
-            {theme === 'dark' ? <Sun /> : <Moon />}
-          </button>
+      <div className="w-[90vw] min-w-[320px] max-w-5xl items-center justify-center flex-col gap-8 flex">
+        <div className="flex gap-4 items-center w-full">
           {buttons?.map((item, index) => (
             <button
               key={index}
-              className="p-2 rounded bg-gray-700 transition-colors hover:bg-gray-800 text-gray-100"
+              className="p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 text-gray-100"
               onClick={item.onClick}
             >
               {item.icon}
@@ -52,29 +43,31 @@ const LayoutDefault: FC<LayoutDefaultProps> = ({
         </div>
         <Comp
           className={clsx(
-            'min-h-[50vh] rounded p-16 flex items-center justify-center flex-col gap-4 shadow-sm',
+            'min-h-[50vh] rounded p-4 flex items-center justify-center flex-col gap-4 shadow-sm w-full overflow-auto relative',
             className,
             theme === 'light'
               ? 'bg-gray-100 text-gray-900'
               : 'bg-gray-800 text-gray-100 dark'
           )}
+          style={{
+            maxWidth: screenSize,
+            ...style,
+          }}
           {...rest}
         >
-          {/* ========================= TEST AREA ========================= */}
           {children}
-          {/* ========================= TEST AREA ========================= */}
         </Comp>
-        {/* {isShowingTerminal && (
-          <pre className="bg-gray-700 py-2 px-4 rounded text-gray-100 relative min-h-12 text-sm mx-2">
-            {JSON.stringify(watch(), null, 2)}
+        {terminal && (
+          <pre className="bg-gray-800  p-4 rounded text-gray-400 relative min-h-12 text-xs w-full">
+            {JSON.stringify(terminal, null, 2)}
             <button
-              className="absolute top-2 right-2 p-2 rounded bg-gray-800 transition-colors hover:bg-gray-900"
-              onClick={() => console.log(getValues())}
+              className="absolute top-2 right-2 p-2 rounded bg-gray-800 transition-colors hover:bg-gray-700 border border-gray-700"
+              onClick={() => console.log(terminal)}
             >
               <Terminal />
             </button>
           </pre>
-        )} */}
+        )}
       </div>
     </div>
   )
