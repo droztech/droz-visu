d216e1244c95da69ca42fa83cad5503b5996a897
YaGRRusso <yagrrusso@gmail.com>
2023-07-03 15:24:56 -0300

    select fix

--- packages/visu/src/library/Select/Item.tsx
+++ packages/visu/src/library/Select/Item.tsx
@@ -2,7 +2,7 @@ import * as RadixSelect from '@radix-ui/react-select'
 import { clsx } from 'clsx'
 import { forwardRef } from 'react'
 
-export interface SelectItemProps extends RadixSelect.SelectItemProps {}
+export type SelectItemProps = RadixSelect.SelectItemProps
 
 const SelectItem = forwardRef<HTMLInputElement, SelectItemProps>(
   ({ children, className, ...rest }, forwardedRef) => {
@@ -10,15 +10,15 @@ const SelectItem = forwardRef<HTMLInputElement, SelectItemProps>(
       <RadixSelect.Item
         ref={forwardedRef}
         className={clsx(
-          'data-[state=checked]:text-primary rounded-lg transition-colors px-3 py-2 cursor-pointer hover:bg-gray-300 hover:text-primary active:outline-none focus:outline-none focus:bg-gray-300 focus:text-primary data-[disabled]:pointer-events-none data-[disabled]:text-gray overflow-hidden text-ellipsis whitespace-nowrap',
-          className
+          'data-[state=checked]:text-primary hover:text-primary focus:text-primary data-[disabled]:text-gray cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-3 py-2 transition-colors hover:bg-gray-300 focus:bg-gray-300 focus:outline-none active:outline-none data-[disabled]:pointer-events-none',
+          className,
         )}
         {...rest}
       >
         <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
       </RadixSelect.Item>
     )
-  }
+  },
 )
 
 SelectItem.displayName = 'Select.Item'
