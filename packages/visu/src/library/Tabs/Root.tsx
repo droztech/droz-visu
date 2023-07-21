04def519358d953ab23194961472ac49f3ac0887
Leandro Pedroso <lpedroso@meudroz.com>
2023-07-21 10:53:48 -0300

    Estilização do componente Tabs

--- packages/visu/src/library/Tabs/Root.tsx
+++ packages/visu/src/library/Tabs/Root.tsx
@@ -1,27 +1,28 @@
 import { Root, List, TabsProps } from '@radix-ui/react-tabs'
-
-import { Ref, forwardRef, useImperativeHandle, useRef } from "react";
+import clsx from 'clsx'
+import { Ref, forwardRef, useImperativeHandle, useRef } from 'react'
 
 export interface TabsRootProps extends TabsProps {
   triggers: React.ReactNode[]
 }
 
-const TabsRoot = forwardRef<HTMLDivElement, TabsRootProps>((
-  { children, triggers, ...rest }, ref: Ref<HTMLDivElement | null>
-) => {
-  const tabsRef = useRef<HTMLDivElement>(null);
+const TabsRoot = forwardRef<HTMLDivElement, TabsRootProps>(
+  (
+    { children, className, triggers, ...rest },
+    ref: Ref<HTMLDivElement | null>,
+  ) => {
+    const tabsRef = useRef<HTMLDivElement>(null)
 
-  useImperativeHandle(ref, () => tabsRef.current);
+    useImperativeHandle(ref, () => tabsRef.current)
 
-  return (
-    <Root ref={tabsRef} {...rest}>
-      <List className='flex w-full flex-row'>
-        {triggers}
-      </List>
-      {children}
-    </Root>
-  )
-})
+    return (
+      <Root className={clsx([className])} ref={tabsRef} {...rest}>
+        <List className="flex w-full flex-row">{triggers}</List>
+        {children}
+      </Root>
+    )
+  },
+)
 
 TabsRoot.displayName = 'Tabs.Root'
 
--- packages/visu/src/library/Tabs/Trigger.tsx
+++ packages/visu/src/library/Tabs/Trigger.tsx
@@ -1,32 +1,54 @@
-import { TabsTrigger as RadixTabsTrigger, TabsTriggerProps as RadixTabsTriggerProps } from '@radix-ui/react-tabs'
+import {
+  TabsTrigger as RadixTabsTrigger,
+  TabsTriggerProps as RadixTabsTriggerProps,
+} from '@radix-ui/react-tabs'
 import clsx from 'clsx'
 import { ReactNode, Ref, forwardRef, useImperativeHandle, useRef } from 'react'
 
-export interface TabsTriggerProps extends Omit<RadixTabsTriggerProps, 'children'> {
-  icon?: ReactNode
+export interface TabsTriggerProps
+  extends Omit<RadixTabsTriggerProps, 'children'> {
+  icon: ReactNode
   title: string
 }
 
-const TabsTrigger = forwardRef<HTMLButtonElement, TabsTriggerProps>((
-  { className, icon, title, ...rest}, ref: Ref<HTMLButtonElement | null>
-) => {
-  const triggerRef = useRef<HTMLButtonElement>(null)
+const TabsTrigger = forwardRef<HTMLButtonElement, TabsTriggerProps>(
+  ({ className, icon, title, ...rest }, ref: Ref<HTMLButtonElement | null>) => {
+    const triggerRef = useRef<HTMLButtonElement>(null)
 
-  useImperativeHandle(ref, () => triggerRef.current)
+    useImperativeHandle(ref, () => triggerRef.current)
 
-  return (
-    <RadixTabsTrigger
-      className={clsx([className, 'group w-full box-content outline-none flex flex-row h-7 items-center justify-center gap-2 data-[state=active]:border-b-4 data-[state=active]:border-primary-400'])}
-      ref={triggerRef}
-      {...rest}
-    >
-      <div className='w-4 h-4 [&_svg]:h-full [&_svg]:w-full'>
-        {icon}
-      </div>
-      <span className='hidden group-data-[state=active]:block sm:block'>{title}</span>
-    </RadixTabsTrigger>
-  )
-})
+    return (
+      <RadixTabsTrigger
+        className={clsx([
+          className,
+          'group box-content flex h-8 flex-row items-start justify-center  border-b border-gray-300 px-4 outline-none',
+          'hover:h-7 hover:border-b-4 hover:border-primary-200 data-[state=active]:h-7 data-[state=active]:w-full data-[state=active]:border-b-4 data-[state=active]:border-primary-400 sm:w-full',
+        ])}
+        ref={triggerRef}
+        {...rest}
+      >
+        <div className="flex h-5 flex-row items-center justify-center gap-2">
+          <div
+            className={clsx([
+              'h-4 w-4 text-gray-700',
+              'group-data-[state=active]:text-primary-400 [&_svg]:h-full [&_svg]:w-full',
+            ])}
+          >
+            {icon}
+          </div>
+          <span
+            className={clsx([
+              'hidden text-sm text-gray-700',
+              'group-data-[state=active]:block group-data-[state=active]:text-gray-900 sm:block',
+            ])}
+          >
+            {title}
+          </span>
+        </div>
+      </RadixTabsTrigger>
+    )
+  },
+)
 
 TabsTrigger.displayName = 'Tabs.Trigger'
 
