e66a90a6ac8c7a10f23bab633d86d57b4b30d9ef
Leandro Pedroso <lpedroso@meudroz.com>
2023-08-10 18:56:40 -0300

    feat: added click to Tooltip.Hover | added other useful props to Tooltip.Hover

--- packages/visu/src/library/Tooltip/Hover.tsx
+++ packages/visu/src/library/Tooltip/Hover.tsx
@@ -1,13 +1,25 @@
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 import { Position } from '@types'
 import { clsx } from 'clsx'
-import { FC, HTMLAttributes, ReactNode } from 'react'
+import {
+  FC,
+  HTMLAttributes,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
 
 export interface TooltipHoverProps
   extends Omit<HTMLAttributes<HTMLSpanElement>, 'content'> {
   // Optional because we can't remove `text` until the next major release
   content?: ReactNode
+  defaultOpen?: boolean
+  delayDuration?: number
+  open?: boolean
   side?: Position
+
+  onOpenChange?: (open: boolean) => void
   /**
    * @deprecated Use `content` instead. Will be removed in the next major release
    */
@@ -18,14 +30,41 @@ const TooltipHover: FC<TooltipHoverProps> = ({
   children,
   className,
   content,
+  defaultOpen = false,
+  delayDuration = 300,
+  open = false,
+  onOpenChange,
   side,
   text,
   ...rest
 }) => {
+  const [tooltipOpen, setTooltipOpen] = useState(open)
+
+  const handleOpenChange = useCallback(
+    (value: boolean) => {
+      setTooltipOpen(value)
+      if (onOpenChange) onOpenChange(value)
+    },
+    [onOpenChange],
+  )
+
+  useEffect(() => {
+    setTooltipOpen(open)
+  }, [open])
+
   return (
     <RadixTooltip.Provider>
-      <RadixTooltip.Root>
-        <RadixTooltip.Trigger className="z-10" asChild>
+      <RadixTooltip.Root
+        defaultOpen={defaultOpen}
+        delayDuration={delayDuration}
+        open={tooltipOpen}
+        onOpenChange={(value) => handleOpenChange(value)}
+      >
+        <RadixTooltip.Trigger
+          className="z-10"
+          asChild
+          onClick={() => setTooltipOpen(true)}
+        >
           {children}
         </RadixTooltip.Trigger>
         <RadixTooltip.Portal className="hidden">
