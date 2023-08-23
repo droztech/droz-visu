ab6448b1357b01e7693eecda1cc6c1a925ec8502
Leandro Pedroso <leandroppf@gmail.com>
2023-08-23 20:07:59 -0300

    test: Added Dialog.Root tests

---
+++ packages/visu/src/__tests__/Dialog/Root.test.tsx
@@ -0,0 +1,23 @@
+import { Dialog } from '@library'
+import { render, screen } from '@testing-library/react'
+
+const dialogTestId = 'dialog-test-id'
+
+describe('DialogRoot tests', () => {
+  it('Should have the displayName equal to "Dialog.Root"', () => {
+    expect(Dialog.Root.displayName).toEqual('Dialog.Root')
+  })
+
+  it('Should render a Dialog.Root element', () => {
+    render(
+      <Dialog.Root>
+        <Dialog.Trigger data-testid={dialogTestId}>
+          <button>Modal</button>
+        </Dialog.Trigger>
+      </Dialog.Root>,
+    )
+    const element = screen.getByTestId(dialogTestId)
+
+    expect(element).toBeInTheDocument()
+  })
+})
