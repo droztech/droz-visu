438037d9cf566570cce96f649a8b3f5b84b51428
Leandro Pedroso <leandroppf@gmail.com>
2023-10-16 20:26:55 -0300

    chore: monorepo configs + Visu style in @droz-js/visu-docs

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,6 +2,6 @@ import VisuTailwindcssPlugin from '@droz-js/visu/dist/theme-plugin'
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}', '../../node_modules/@droz-js/visu/dist/visu.es.js'],
   plugins: [VisuTailwindcssPlugin],
 } satisfies Config
