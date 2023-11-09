66e00afdc785838063a0ee0818d712db0bb8d134
Yago Russo <yagrrusso@gmail.com>
2023-11-09 15:07:32 -0300

    chore: easy export all component exported types

--- packages/visu/src/library/index.ts
+++ packages/visu/src/library/index.ts
@@ -3,101 +3,67 @@ import './styles/index.css'
 export * from './types'
 
 export { default as Avatar } from './Avatar'
-export type {
-  AvatarFallbackProps,
-  AvatarImageProps,
-  AvatarRootProps,
-} from './Avatar'
+export type * from './Avatar'
 
 export { default as Button } from './Button'
-export type { ButtonIconProps, ButtonRootProps } from './Button'
+export type * from './Button'
 
 export { default as Checkbox } from './Checkbox'
-export type { CheckboxIndicatorProps, CheckboxRootProps } from './Checkbox'
+export type * from './Checkbox'
 
 export { default as Dialog } from './Dialog'
-export type {
-  DialogBodyProps,
-  DialogCloseProps,
-  DialogContentProps,
-  DialogFooterProps,
-  DialogHeaderProps,
-  DialogRootProps,
-  DialogTriggerProps,
-} from './Dialog'
+export type * from './Dialog'
 
 export { default as Divider } from './Divider'
-export type { DividerProps } from './Divider'
+export type * from './Divider'
 
 export { default as File } from './File'
-export type { FileProps } from './File'
+export type * from './File'
 
 export { default as Flex } from './Flex'
-export type { FlexColProps, FlexRootProps, FlexRowProps } from './Flex'
+export type * from './Flex'
 
 export { default as FloatCard } from './FloatCard'
-export type {
-  FloatCardContentProps,
-  FloatCardRootProps,
-  FloatCardTriggerProps,
-} from './FloatCard'
+export type * from './FloatCard'
 
 export { default as Icon } from './Icon'
-export type { IconProps } from './Icon'
+export type * from './Icon'
 
 export { default as Input } from './Input'
-export type { InputIconProps, InputInputProps, InputRootProps } from './Input'
+export type * from './Input'
 
 export { default as Link } from './Link'
-export type { LinkProps } from './Link'
+export type * from './Link'
 
 export { default as Loading } from './Loading'
-export type { LoadingProps } from './Loading'
+export type * from './Loading'
 
 export { default as Menu } from './Menu'
-export type {
-  MenuRootProps,
-  MenuButtonProps,
-  MenuGroupProps,
-  MenuBodyProps,
-  MenuHeaderProps,
-} from './Menu'
+export type * from './Menu'
 
 export { default as OTPInput } from './OTPInput'
-export type { OTPInputProps } from './OTPInput'
+export type * from './OTPInput'
 
 export { default as Pagination } from './Pagination'
-export type { PaginationProps } from './Pagination'
+export type * from './Pagination'
 
 export { default as Radio } from './Radio'
-export type { RadioItemProps, RadioRootProps } from './Radio'
+export type * from './Radio'
 
 export { default as Select } from './Select'
-export type { SelectRootProps, SelectItemProps } from './Select'
+export type * from './Select'
 
 export { default as Sidebar } from './Sidebar'
-export type {
-  SidebarRootProps,
-  SidebarLinkProps,
-  SidebarButtonProps,
-  SidebarGroupProps,
-  SidebarLayoutProps,
-  SidebarBodyProps,
-  SidebarHeaderProps,
-} from './Sidebar'
+export type * from './Sidebar'
 
 export { default as Switch } from './Switch'
-export type { SwitchProps } from './Switch'
+export type * from './Switch'
 
 export { default as Tabs } from './Tabs'
-export type { TabsContentProps, TabsRootProps, TabsTriggerProps } from './Tabs'
+export type * from './Tabs'
 
 export { default as Tooltip } from './Tooltip'
-export type {
-  TooltipCloseProps,
-  TooltipStepProps,
-  TooltipHoverProps,
-} from './Tooltip'
+export type * from './Tooltip'
 
 export { default as ToastProvider, useToast } from './Toast'
-export type { ToastProviderProps, ToastRootProps } from './Toast'
+export type * from './Toast'
