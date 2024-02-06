47f9969ad4cb36b7d39dfed3e6b43e74ae6d6dde
Yago Russo <yagrrusso@gmail.com>
2024-02-06 10:31:54 -0300

    release: 2.6.2

--- scripts/versioning.js
+++ scripts/versioning.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const versioning = mainPackageJson.version
 console.log('Bumping all packages to version:', versioning)
 
-glob.sync('./*/*/package.json').forEach((location) => {
+glob.sync('**/package.json').forEach((location) => {
   const packageJsonStr = fs.readFileSync(location).toString()
   const packageJson = JSON.parse(packageJsonStr)
   packageJson.version = versioning
