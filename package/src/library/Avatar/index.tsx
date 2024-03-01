c75fee0bdd0d0be87719ff3926ce06bfac98be67
Yago Russo <yagrrusso@gmail.com>
2024-03-01 16:20:09 -0300

    chore: variant example

--- package/src/library/Avatar/index.tsx
+++ package/src/library/Avatar/index.tsx
@@ -6,40 +6,40 @@ import { FC, ReactNode } from 'react'
 const colorVariants = {
   primary: {
     root: 'text-primary border-primary hover:border-primary-300 active:border-primary-200 hover:text-primary-300 active:text-primary-300',
-    bg:'bg-primary-200',
-    status:'border-primary-700'
+    bg: 'bg-primary-200',
+    status: 'border-primary-700',
   },
   secondary: {
     root: 'text-secondary border-secondary hover:border-secondary-300 active:border-secondary-200 hover:text-secondary-300 active:text-secondary-300',
-    bg:'bg-secondary-200',
-    status:'border-secondary-700'
-  }
+    bg: 'bg-secondary-200',
+    status: 'border-secondary-700',
+  },
 }
 
 const sizeVariants = {
   sm: {
     root: 'w-6 h-6 text-xs',
     fallback: 'text-xs p-0',
-    status:'w-1 h-1'
+    status: 'w-1 h-1',
   },
   md: {
-    root:'w-10 h-10 text-md',
-    fallback:'text-md p-1',
-    status:'w-2 h-2'
+    root: 'w-10 h-10 text-md',
+    fallback: 'text-md p-1',
+    status: 'w-2 h-2',
   },
   lg: {
-    root:'w-12 h-12 text-lg',
-    fallback:'text-lg p-1',
-    status:'w-3 h-3'
+    root: 'w-12 h-12 text-lg',
+    fallback: 'text-lg p-1',
+    status: 'w-3 h-3',
   },
 }
 
 const statusVariants = {
   success: {
-    root:'bg-success'
+    root: 'bg-success',
   },
   error: {
-    root:'bg-error'
+    root: 'bg-error',
   },
 }
 
@@ -79,9 +79,9 @@ const Avatar: FC<AvatarProps> = ({
         <div
           className={cn(
             'absolute right-0 top-0 rounded-full border-2',
-            status &&statusVariants[status].root,
-           sizeVariants[size].status,
-           colorVariants[color].status,
+            status && statusVariants[status].root,
+            sizeVariants[size].status,
+            colorVariants[color].status,
           )}
         />
       )}
--- package/src/library/Button/index.tsx
+++ package/src/library/Button/index.tsx
@@ -5,32 +5,53 @@ import { cn } from '@/src/utils/class-merge.helper'
 import { Slot } from '@radix-ui/react-slot'
 import { ButtonHTMLAttributes, forwardRef, useMemo } from 'react'
 
-const variantClassVariants = {
-  neutral:
-    'bg-gray-100 border-none text-gray-900 shadow-sm hover:bg-gray-200 active:bg-gray-400',
-  primary:
-    'bg-primary border-none text-gray-100 hover:bg-primary-500 active:bg-primary-600',
-  secondary:
-    'bg-secondary border-none text-gray-100 hover:bg-secondary-500 active:bg-secondary-600',
+const disabledVariants = {
+  root: {
+    variant: 'pointer-events-none bg-gray text-gray-500',
+    ghost:
+      'pointer-events-none border border-gray text-gray-500 bg-transparent',
+    light: 'pointer-events-none text-gray bg-transparent',
+  },
 }
 
-const ghostClassVariants = {
-  neutral: 'border border-solid bg-transparent border-gray-600 text-gray-900',
-  primary: 'border border-solid bg-transparent border-primary text-primary',
-  secondary:
-    'border border-solid bg-transparent border-secondary text-secondary',
+const colorVariants = {
+  neutral: {
+    root: {
+      variant:
+        'bg-gray-100 border-none text-gray-900 shadow-sm hover:bg-gray-200 active:bg-gray-400',
+      ghost: 'border border-solid bg-transparent border-gray-600 text-gray-900',
+      light: 'bg-transparent text-gray-900',
+    },
+  },
+  primary: {
+    root: {
+      variant:
+        'bg-primary border-none text-gray-100 hover:bg-primary-500 active:bg-primary-600',
+      ghost: 'border border-solid bg-transparent border-primary text-primary',
+      light: 'bg-transparent text-primary',
+    },
+  },
+  secondary: {
+    root: {
+      variant:
+        'bg-secondary border-none text-gray-100 hover:bg-secondary-500 active:bg-secondary-600',
+      ghost:
+        'border border-solid bg-transparent border-secondary text-secondary',
+      light: 'bg-transparent text-secondary',
+    },
+  },
 }
 
-const lightClassVariants = {
-  neutral: 'bg-transparent text-gray-900',
-  primary: 'bg-transparent text-primary',
-  secondary: 'bg-transparent text-secondary',
-}
-
-const sizeClassVariants = {
-  sm: 'py-2.5 px-6 text-sm min-h-10',
-  md: 'py-3 px-10 min-h-12',
-  lg: 'py-9 px-12 text-lg min-h-28 [&_div]:h-10 [&_div]:w-10',
+const sizeVariants = {
+  sm: {
+    root: 'py-2.5 px-6 text-sm min-h-10',
+  },
+  md: {
+    root: 'py-3 px-10 min-h-12',
+  },
+  lg: {
+    root: 'py-9 px-12 text-lg min-h-28 [&_div]:h-10 [&_div]:w-10',
+  },
 }
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -38,8 +59,8 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   ghost?: boolean
   light?: boolean
   loading?: boolean
-  size?: keyof typeof sizeClassVariants
-  variant?: keyof typeof variantClassVariants
+  size?: keyof typeof sizeVariants
+  variant?: keyof typeof colorVariants
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -63,21 +84,20 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 
     const rootClass = useMemo(() => {
       if (isDisabled) {
-        if (ghost)
-          return 'pointer-events-none border border-gray text-gray-500 bg-transparent'
-        if (light) return 'pointer-events-none text-gray bg-transparent'
-        return 'pointer-events-none bg-gray text-gray-500'
+        if (ghost) return disabledVariants.root.ghost
+        if (light) return disabledVariants.root.light
+        return disabledVariants.root.variant
       }
-      if (light) return lightClassVariants[variant]
-      if (ghost) return ghostClassVariants[variant]
-      return variantClassVariants[variant]
+      if (light) return colorVariants[variant].root.light
+      if (ghost) return colorVariants[variant].root.ghost
+      return colorVariants[variant].root.variant
     }, [ghost, isDisabled, light, variant])
 
     return (
       <RootComponent
         className={cn(
           'flex cursor-pointer items-center justify-center gap-2.5 rounded-lg transition-all [&_svg]:w-6',
-          light ? 'p-2' : sizeClassVariants[size],
+          light ? 'p-2' : sizeVariants[size].root,
           rootClass,
           className,
         )}
