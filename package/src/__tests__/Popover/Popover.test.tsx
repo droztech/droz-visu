dfec3a921315603ba858553bf0f19daa5d9fce1b
Francis Targanski <francis@targanski.com>
2024-03-21 15:02:01 -0300

    Adicionado componente Popover e seus tipos

---
+++ package/src/__tests__/Popover/Popover.test.tsx
@@ -0,0 +1,25 @@
+import { Popover } from '@library'
+
+import { render, screen } from '@testing-library/react'
+// eslint-disable-next-line import/no-named-as-default
+import userEvent from '@testing-library/user-event'
+
+describe('Popover tests', () => {
+  it('Should render a Popover component with icon', () => {
+    render(<Popover icon={<span>Icon</span>} />)
+    const iconElement = screen.getByText('Icon')
+    expect(iconElement).toBeInTheDocument()
+  })
+
+  it('Should trigger popover when clicked on icon', async () => {
+    render(
+      <Popover icon={<span>Icon</span>} placeholder="Placeholder">
+        <div>Children</div>
+      </Popover>,
+    )
+    const iconElement = screen.getByText('Icon')
+    userEvent.click(iconElement)
+    const popoverContent = await screen.findByText('Children')
+    expect(popoverContent).toBeInTheDocument()
+  })
+})
