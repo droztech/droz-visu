1cb98f04a12b9599b5c1325c1e938c09b6917435
Francis Targanski <francis@targanski.com>
2024-03-22 11:03:37 -0300

    Atualizar componente Popover.Root

--- docs/src/stories/Popover/Root.Stories.tsx
+++ docs/src/stories/Popover/Root.Stories.tsx
@@ -1,45 +1,30 @@
-import { Avatar, Popover, PopoverRootProps } from '@droz-js/visu'
-import { ShareFat } from '@phosphor-icons/react'
+import { Popover, PopoverRootProps } from '@droz-js/visu'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<PopoverRootProps> = {
   title: 'Popover/Root',
-  component: Popover.Content,
+  component: Popover.Root,
   argTypes: {
     children: {
+      control: 'none',
       table: {
         type: {
           summary: 'React.ReactNode',
         },
       },
+      type: { name: 'other', required: false, value: 'React.ReactNode' },
     },
   },
   args: {
     children: '',
   },
-}
+} as Meta<PopoverRootProps>
 
 export default meta
 type PopoverRootStory = StoryObj<PopoverRootProps>
 
 export const Comum: PopoverRootStory = {
   render: (args) => {
-    return (
-      <Popover.Root {...args}>
-        <Popover.Trigger>
-          <ShareFat size={32} />
-        </Popover.Trigger>
-        <Popover.Content>
-          <div className="flex flex-col">
-            <Avatar fallback="FC" />
-
-            <span>
-              O Root é apenas o container do componente. Verifique o código na
-              docs.
-            </span>
-          </div>
-        </Popover.Content>
-      </Popover.Root>
-    )
+    return <Popover.Root {...args}></Popover.Root>
   },
 }
