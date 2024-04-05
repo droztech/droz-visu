5f97bea49d01cf2b532de9cf447a17878da09f3b
Yago Russo <yagrrusso@gmail.com>
2024-04-05 16:12:42 -0300

    chore: select weight

--- package/src/library/Select/Item.tsx
+++ package/src/library/Select/Item.tsx
@@ -11,7 +11,7 @@ const SelectItem = forwardRef<HTMLInputElement, SelectItemProps>(
       <RadixSelect.Item
         ref={forwardedRef}
         className={cn(
-          'cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-3 py-2 transition-colors hover:bg-gray-300 hover:text-primary focus:bg-gray-300 focus:text-primary focus:outline-none active:outline-none data-[disabled]:pointer-events-none data-[disabled]:text-gray data-[state=checked]:text-primary',
+          'cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-3 py-2 font-light transition-colors hover:bg-gray-300 hover:text-primary focus:bg-gray-300 focus:text-primary focus:outline-none active:outline-none data-[disabled]:pointer-events-none data-[state=checked]:font-normal data-[disabled]:text-gray data-[state=checked]:text-primary',
           className,
         )}
         {...rest}
